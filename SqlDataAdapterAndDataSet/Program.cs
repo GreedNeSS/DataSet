f2b412f9fd0719932b5c3b53daa9beb1a9c7b20a
GreedNeSS <ruslan199126@gmail.com>
2022-03-23 18:57:19 +0300

    Добавьте файлы проекта.

---
+++ SqlDataAdapterAndDataSet/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+Console.WriteLine("***** SqlDataAdapter And DataSet *****");
+
+string connectString = "Server=localhost;Database=adonetdb;Trusted_Connection=True;Encrypt=False";
+string sql = "SELECT * FROM Users";
+
+using (SqlConnection connection = new SqlConnection(connectString))
+{
+    DataSet ds = new DataSet();
+    SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+    adapter.Fill(ds);
+
+    foreach (DataTable table in ds.Tables)
+    {
+        foreach (DataColumn column in table.Columns)
+        {
+            Console.Write($"{column.ColumnName}\t");
+        }
+
+        Console.WriteLine();
+
+        foreach (DataRow row in table.Rows)
+        {
+            foreach (object cell in row.ItemArray)
+            {
+                Console.Write($"{cell}\t");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.ReadLine();
